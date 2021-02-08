28ba3be65c4a52db134510b975b3a9ebb766533f
Bermarte <bernardo.martelli@gmail.com>
2021-02-08 10:07:02 +0100

    change time

--- src/handlers/shortBreakHandler.js
+++ src/handlers/shortBreakHandler.js
@@ -4,8 +4,8 @@ import { BreakClass } from '../classes/Break.js';
 
 export function shortBreak(){
 
-    //const breakObj = new BreakClass('short break', 'green', '05:00');
-    const breakObj = new BreakClass('short break', 'green', '00:05');
+    const breakObj = new BreakClass('short break', 'green', '05:00');
+    
     breakObj.render();
 
 }
